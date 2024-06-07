c42f91e22f681e58c7062058c87e0065b227d6e4
abahonjelearn <abahonjelearn@gmail.com>
2024-06-07 19:28:52 +0700

    menambahkan pragraph kedua

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nihil pariatur expedita porro vitae totam? Veniam at ipsa debitis eligendi, in deserunt quas eveniet ab saepe labore magni error illo.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est nam numquam praesentium consequatur itaque rem repudiandae, ducimus sunt quidem blanditiis facere possimus ex velit, commodi fuga distinctio incidunt tempora!</p>
 </body>
 </html>
